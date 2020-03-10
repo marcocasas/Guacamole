b7e91b596aad545f427dd0433571255ff745db0a
marcocasas <marcoacasasm@gmail.com>
2020-03-10 10:03:06 -0600

    Primer intento para enviar via TCP el tiro del jugador

--- src/GameInt.java
+++ src/GameInt.java
@@ -1,13 +1,13 @@
+
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
-import java.net.SocketException;
+import java.io.*;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JRadioButton;
 import javax.swing.JDialog;
@@ -16,6 +16,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class GameInt implements Runnable, ActionListener {
+
     JDialog dialog = new JDialog();
     int number = 0;
     JLabel message = new JLabel();
@@ -24,12 +25,14 @@ public class GameInt implements Runnable, ActionListener {
     JRadioButton pos4 = new JRadioButton("(   )"), pos5 = new JRadioButton("(   )");
     JRadioButton pos6 = new JRadioButton("(   )"), pos7 = new JRadioButton("(   )");
     JRadioButton pos8 = new JRadioButton("(   )");
-    
+
     String messageStr = "Topo en la posicion: ";
     boolean runProcess = true;
 
+    private DataOutputStream out;
+    
     public GameInt() {
-        dialog.setLayout(new GridLayout(4,1));
+        dialog.setLayout(new GridLayout(4, 1));
         pos0.addActionListener(this);
         pos1.addActionListener(this);
         pos2.addActionListener(this);
@@ -39,15 +42,15 @@ public class GameInt implements Runnable, ActionListener {
         pos6.addActionListener(this);
         pos7.addActionListener(this);
         pos8.addActionListener(this);
-        
+
         JPanel btnPanel = new JPanel();
         JPanel btnPanel2 = new JPanel();
         JPanel btnPanel3 = new JPanel();
-        
+
         btnPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
         btnPanel2.setLayout(new FlowLayout(FlowLayout.CENTER));
         btnPanel3.setLayout(new FlowLayout(FlowLayout.CENTER));
-        
+
         btnPanel.add(pos0);
         btnPanel.add(pos1);
         btnPanel.add(pos2);
@@ -57,19 +60,19 @@ public class GameInt implements Runnable, ActionListener {
         btnPanel3.add(pos6);
         btnPanel3.add(pos7);
         btnPanel3.add(pos8);
-        
+
         dialog.add(message, "Center");
         dialog.add(btnPanel, "Center");
         dialog.add(btnPanel2, "Center");//
         dialog.add(btnPanel3, "Center");//
-        
+
         dialog.setPreferredSize(new Dimension(300, 300));
-        
+
         dialog.pack();
-        
+
         dialog.setVisible(true);
         dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        
+
         new Thread(this).start();
     }
 
@@ -77,99 +80,161 @@ public class GameInt implements Runnable, ActionListener {
     public void run() {
         int puntos = 0;
         int prevPos = 0;
-        
-        JRadioButton[] posiciones = {pos0,pos1,pos2,pos3,pos4,pos5,pos6,pos7,pos8};
-        
+
+        int serverPort = 7896; //puerto servidor TCP
+        Socket stcp = null; //socket tcp
+
+        MulticastSocket s = null; //socket multicast
+
+        JRadioButton[] posiciones = {pos0, pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8};
+
         while (runProcess) {
-        
-            MulticastSocket s = null;
             try {
                 InetAddress group = InetAddress.getByName("228.28.6.13"); // destination multicast group; misma direccion que el sender
-	    	s = new MulticastSocket(6789);
-	   	s.joinGroup(group); 
+                s = new MulticastSocket(6789);
+                s.joinGroup(group);
 
-	    	byte[] buffer = new byte[1000];
- 	   	//for(int i=0; i< 3; i++) {
-                    //System.out.println("Waiting for messages");
-                DatagramPacket messageIn = 
-                    new DatagramPacket(buffer, buffer.length);
+                byte[] buffer = new byte[1000];
+                //for(int i=0; i< 3; i++) {
+                //System.out.println("Waiting for messages");
+                DatagramPacket messageIn
+                        = new DatagramPacket(buffer, buffer.length);
+
+                stcp = new Socket("localhost", serverPort);
+                //   s = new Socket("127.0.0.1", serverPort);    
+                DataInputStream in = new DataInputStream(stcp.getInputStream());
+                this.out = new DataOutputStream(stcp.getOutputStream());
                 
-                while(puntos < 50) {
+                while (puntos < 50) {
                     s.receive(messageIn);
                     //System.out.println("Fecha actualizada: " + new String(messageIn.getData())+ " de: "+ messageIn.getAddress());
                     //}
                     //System.out.println("Tablero actual: ");
 
                     posiciones[prevPos].setText("(   )");
-                    
-                    byte[] m = messageIn.getData();                    
-                    
-                    for (int i = 0; i<9; i++) {                        
-                        if(m[i] == 1) {
+
+                    byte[] m = messageIn.getData();
+
+                    for (int i = 0; i < 9; i++) {
+                        if (m[i] == 1) {
                             prevPos = i;
                             posiciones[i].setText("ʕ•.•ʔ");
                             message.setText(messageStr + " " + i + "");
                         }
                     }
-                
+
                     //System.out.println("\n");
                     
+                    String data = in.readUTF();
+                    System.out.println("Received: " + data);
+            
                     puntos++;
                     //
                 }
-                
-	    	s.leaveGroup(group);		
-            }
-            catch (SocketException e){
+
+                s.leaveGroup(group);
+            } catch (SocketException e) {
                 System.out.println("Socket: " + e.getMessage());
-            }
-            catch (IOException e){
+            } catch (IOException e) {
                 System.out.println("IO: " + e.getMessage());
+            } finally {
+                if (s != null) {
+                    s.close(); //Cerramos para liberar recursos del serbiddor.
+                }
+                if (stcp != null) {
+                    try {
+                        stcp.close();
+                    } catch (IOException e) {
+                        System.out.println("close:" + e.getMessage());
+                    }
+                }
             }
-            finally {
-               if(s != null) s.close(); //Cerramos para liberar recursos del serbiddor.
-           }
+        }
 
-      runProcess = false;
+        runProcess = false;
     }
-  }
 
-  @Override
-  public void actionPerformed(ActionEvent e) {
-    JRadioButton pressed = (JRadioButton)e.getSource();
-    if (pressed == pos0) {
-        System.out.println("0");
-        pos0.setSelected(false);
-    } else if (pressed == pos1) {
-        System.out.println("1");
-        pos1.setSelected(false);
-    } else if (pressed == pos2) {
-        System.out.println("2");
-        pos2.setSelected(false);
-    } else if (pressed == pos3) {
-        System.out.println("3");
-        pos3.setSelected(false);
-    } else if (pressed == pos4) {
-        System.out.println("4");
-        pos4.setSelected(false);
-    } else if (pressed == pos5) {
-        System.out.println("5");
-        pos5.setSelected(false);
-    } else if (pressed == pos6) {
-        System.out.println("6");
-        pos6.setSelected(false);
-    } else if (pressed == pos7) {
-        System.out.println("7");
-        pos7.setSelected(false);
-    } else if (pressed == pos8) {
-        System.out.println("8");
-        pos8.setSelected(false);
-    } else {
-      dialog.dispose();
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JRadioButton pressed = (JRadioButton) e.getSource();
+        if (pressed == pos0) {
+            try { 
+                out.writeUTF("0");
+            } catch (IOException ex) {
+                Logger.getLogger(GameInt.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("0");
+            pos0.setSelected(false);
+        } else if (pressed == pos1) {
+             try { 
+                out.writeUTF("1");
+            } catch (IOException ex) {
+                Logger.getLogger(GameInt.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("1");
+            pos1.setSelected(false);
+        } else if (pressed == pos2) {
+            try { 
+                out.writeUTF("2");
+            } catch (IOException ex) {
+                Logger.getLogger(GameInt.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("2");
+            pos2.setSelected(false);
+        } else if (pressed == pos3) {
+            try { 
+                out.writeUTF("3");
+            } catch (IOException ex) {
+                Logger.getLogger(GameInt.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("3");
+            pos3.setSelected(false);
+        } else if (pressed == pos4) {
+            try { 
+                out.writeUTF("4");
+            } catch (IOException ex) {
+                Logger.getLogger(GameInt.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("4");
+            pos4.setSelected(false);
+        } else if (pressed == pos5) {
+            try { 
+                out.writeUTF("5");
+            } catch (IOException ex) {
+                Logger.getLogger(GameInt.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("5");
+            pos5.setSelected(false);
+        } else if (pressed == pos6) {
+             try { 
+                out.writeUTF("6");
+            } catch (IOException ex) {
+                Logger.getLogger(GameInt.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("6");
+            pos6.setSelected(false);
+        } else if (pressed == pos7) {
+            try { 
+                out.writeUTF("7");
+            } catch (IOException ex) {
+                Logger.getLogger(GameInt.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("7");
+            pos7.setSelected(false);
+        } else if (pressed == pos8) {
+            try { 
+                out.writeUTF("8");
+            } catch (IOException ex) {
+                Logger.getLogger(GameInt.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("8");
+            pos8.setSelected(false);
+        } else {
+            dialog.dispose();
+        }
     }
-  }
 
-  public static void main(String args[]) {
-    GameInt ean = new GameInt();
-  }
+    public static void main(String args[]) {
+        GameInt ean = new GameInt();
+    }
 }
--- src/MulticastSenderPeer.java
+++ src/MulticastSenderPeer.java
@@ -4,14 +4,8 @@
  * and open the template in the editor.
  */
 
-
-
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
-import java.net.SocketException;
-import java.util.Date;
+import java.io.*;
+import java.net.*;
 import java.util.Random;
 
 /**
@@ -19,48 +13,94 @@ import java.util.Random;
  * @author JGUTIERRGARC
  */
 public class MulticastSenderPeer {
-    	public static void main(String args[]) throws InterruptedException{ 
-  	 
-	MulticastSocket s =null;
-   	 try {
-                
-                InetAddress group = InetAddress.getByName("228.28.6.13"); // destination multicast group 
-	    	s = new MulticastSocket(6789); //Puerto en el que me estoy uniendo al grupo.
-	   	s.joinGroup(group); 
+
+    public static void main(String args[]) throws InterruptedException {
+
+        MulticastSocket s = null;
+        try {
+
+            InetAddress group = InetAddress.getByName("228.28.6.13"); // destination multicast group 
+            s = new MulticastSocket(6789); //Puerto en el que me estoy uniendo al grupo.
+            s.joinGroup(group);
+
+            int serverPort = 7896; //TCP
+            ServerSocket listenSocket = new ServerSocket(serverPort); //TCP
+
+            int pos = 0; //Posicion en la que saldra el topo. Será el indice del arreglo
+            int prevPos; //Posicion previa
+            byte[] posiciones = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+            while (true) {
+                System.out.println("Waiting for players...");
+                Socket clientSocket = listenSocket.accept();  // Listens for a connection to be made to this socket and accepts it. The method blocks until a connection is made. 
+                Connection c = new Connection(clientSocket);
+                c.start();
+                //}
+
                 //s.setTimeToLive(10);
-                System.out.println("Messages' TTL (Time-To-Live): " + s.getTimeToLive());
+                //System.out.println("Messages' TTL (Time-To-Live): " + s.getTimeToLive());
                 //String myMessage="Hello";
                 //String currentDate;
-                
-                int pos = 0; //Posicion en la que saldra el topo. Será el indice del arreglo
-                int prevPos; //Posicion previa
-                byte[] posiciones = {0,0,0,0,0,0,0,0,0};
-                
-                while(true) {
-                    //currentDate = (new Date()).toString();
-                    //byte [] m = myMessage.getBytes();
-                    prevPos = pos;
-                    pos = new Random().nextInt(9);
-                    System.out.println("Topo en " + pos);
-                    posiciones[prevPos] = 0;
-                    posiciones[pos] = 1;
-                    //byte [] m = posiciones;
-                    DatagramPacket messageOut = 
-                            new DatagramPacket(posiciones, posiciones.length, group, 6789);
-                    s.send(messageOut);
-                    
-                    Thread.sleep(3000);
-                }
-	    	//s.leaveGroup(group);		
- 	    }
-         catch (SocketException e){
-             System.out.println("Socket: " + e.getMessage());
-	 }
-         catch (IOException e){
-             System.out.println("IO: " + e.getMessage());
-         }
-	 finally {
-            if(s != null) s.close();
+                //while(true) {
+                //currentDate = (new Date()).toString();
+                //byte [] m = myMessage.getBytes();
+                prevPos = pos;
+                pos = new Random().nextInt(9);
+                System.out.println("Topo en " + pos);
+                posiciones[prevPos] = 0;
+                posiciones[pos] = 1;
+                //byte [] m = posiciones;
+                DatagramPacket messageOut
+                        = new DatagramPacket(posiciones, posiciones.length, group, 6789);
+                s.send(messageOut);
+
+                Thread.sleep(3000);
+            }
+            //s.leaveGroup(group);		
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO: " + e.getMessage());
+        } finally {
+            if (s != null) {
+                s.close();
+            }
         }
-    }		     
+    }
+}
+
+class Connection extends Thread {
+
+    DataInputStream in;
+    DataOutputStream out;
+    Socket clientSocket;
+
+    public Connection(Socket aClientSocket) {
+        try {
+            clientSocket = aClientSocket;
+            in = new DataInputStream(clientSocket.getInputStream());
+            out = new DataOutputStream(clientSocket.getOutputStream());
+        } catch (IOException e) {
+            System.out.println("Connection:" + e.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        try {			                 // an echo server
+            String data = in.readUTF();
+            System.out.println("Message received from: " + clientSocket.getRemoteSocketAddress());
+            out.writeUTF(data);
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
 }
